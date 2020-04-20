04c531696391b91bedf56076d763b2fb9739bc7b
Romart <48014093+RoMar19@users.noreply.github.com>
2020-04-20 17:26:40 +0200

    Add files via upload

---
+++ mysql-from-python (1).py
@@ -0,0 +1,21 @@
+import os
+import pymysql
+
+# Get the username from the Cloud9 workspace
+# (modify this variable if running on another environment)
+username = os.getenv('C9_USER')
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user=username,
+                             password='',
+                             db='Chinook')
+
+try:
+    with connection.cursor() as cursor:
+        list_of_names = ['Fred', 'fred']
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});".format(format_strings), list_of_names)
+        connection.commit()    
+finally:
+    connection.close()
